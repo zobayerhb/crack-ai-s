7da9a92a4573bbbbc3fc694911d186f784a92747
zobayerhb <hosenzobayer55@gmail.com>
2025-03-10 00:00:30 +0600

    gemini api key

---
+++ index.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+app.get("/test-ai", async (req, res) => {
+  const prompt = req.query.prompt;
+
+  if (!prompt) {
+    res.send({ message: "Please provide prompt" });
+    return;
+  }
+
+  const result = await model.generateContent(prompt);
+  console.log(result.response.text());
+  res.send({ message: result.response.text() });
+});
+
+
+app.get("/", (req, res) => {
+  res.send({ messag: "Let's crack the ai ......" });
+});
+app.listen(port, () => {
+  console.log("Server running on port ", port);
+});
