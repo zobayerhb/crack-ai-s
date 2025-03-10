9de6959130c00fbefa8e0c529bd47afca8bcf62b
zobayerhb <hosenzobayer55@gmail.com>
2025-03-10 11:32:32 +0600

    chat conversion ai

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 app.get("/test-ai", async (req, res) => {
-  const prompt = req.query.prompt;
+  const prompt = req.query?.prompt;
 
   if (!prompt) {
     res.send({ message: "Please provide prompt" });
@@ -20,6 +20,30 @@ app.get("/test-ai", async (req, res) => {
   res.send({ message: result.response.text() });
 });
 
+app.get("/chat-conversation", async (req, res) => {
+  const prompt = req.query?.prompt;
+
+  if (!prompt) {
+    res.send({ message: "Please provide a prompt in query" });
+    return;
+  }
+
+  const chat = model.startChat({
+    history: [
+      {
+        role: "user",
+        parts: [{ text: "Hello" }],
+      },
+      {
+        role: "model",
+        parts: [{ text: "Great to meet you. What would you like to know?" }],
+      },
+    ],
+  });
+  const result = await chat.sendMessage(prompt);
+  const answer = result.response.text();
+  res.send({ rumorMessage: answer });
+});
 
 app.get("/", (req, res) => {
   res.send({ messag: "Let's crack the ai ......" });
